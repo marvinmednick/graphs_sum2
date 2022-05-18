cda548e056e268f700b2404304624c20a82b2ff6
Marvin Mednick <mmednick@gmail.com>
2022-05-17 23:10:01 -0700

    initial checking, working but loop in incorrect order

---
+++ src/cmd_line.rs
@@ -0,0 +1,78 @@
+extern crate clap;
+
+use clap::{Arg, Command};
+
+#[derive(Debug)]
+pub struct CommandArgs  {
+    pub filename: String,
+    pub start: i32,
+    pub end: i32,
+}
+
+impl CommandArgs  {
+    pub fn new() -> Self {
+        // basic app information
+        let app = Command::new("sum2")
+            .version("1.0")
+            .about("Caculates sum2 for input file")
+            .author("Marvin Mednick");
+
+        // Define the name command line option
+        let filename_option = Arg::new("file")
+            .takes_value(true)
+            .help("Input file name")
+            .required(true);
+
+        // Define the starting value option
+        let start_option = Arg::new("start")
+            .takes_value(true)
+            .allow_hyphen_values(true)
+            .required(true)
+            .help("lowest value for t");
+
+        // Define the starting value option
+        let end_option = Arg::new("end")
+            .takes_value(true)
+            .required(true)
+            .allow_hyphen_values(true)
+            .help("highest value for t");
+
+        // now add in the argument we want to parse
+        let mut app = app.arg(filename_option);
+        app = app.arg(start_option);
+        app = app.arg(end_option);
+
+        // extract the matches
+        let matches = app.get_matches();
+
+        // Extract the actual name
+        let filename = matches.value_of("file")
+            .expect("Filename can't be None, we said it was required");
+
+        let start_str = matches.value_of("start");
+
+        let start = match start_str {
+            None => { println!("Start is None..."); 0},
+            Some(s) => {
+                match s.parse::<i32>() {
+                    Ok(n) => n,
+                    Err(_) => {println!("That's not a number! {}", s); 0},
+                }
+            }
+        };
+        let end_str = matches.value_of("end");
+
+        let end = match end_str {
+            None => { println!("End is None..."); 0},
+            Some(s) => {
+                match s.parse::<i32>() {
+                    Ok(n) => n,
+                    Err(_) => {println!("That's not a number! {}", s); 0},
+                }
+            }
+        };
+        println!("clap args: {} {} {}",filename,start,end); 
+
+        CommandArgs { filename: filename.to_string(), start: start, end: end }
+    }   
+}
---
+++ src/main.rs
@@ -0,0 +1,177 @@
+use std::path::Path;
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+use regex::Regex;
+use std::process::exit;
+
+mod cmd_line;
+use crate::cmd_line::CommandArgs;
+use std::collections::HashSet;
+use std::collections::BTreeSet;
+// extern crate time;
+
+use std::time::Instant;
+
+
+fn main() {
+
+
+    let cmd_line = CommandArgs::new();
+
+    println!("Hello, {:?}!",cmd_line);
+
+    println!("Calulating sum2 for t from {} to {}",cmd_line.start,cmd_line.end);
+  // Create a path to the desired file
+    let path = Path::new(&cmd_line.filename);
+    let display = path.display();
+
+    // Open the path in read-only mode, returns `io::Result<File>`
+    let file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+
+    let mut input_set = BTreeSet::<i64>::new();
+    let mut target_set = HashSet::<i64>::new();
+    let mut result_set = HashSet::<i64>::new();
+    let before = Instant::now();
+    for i in cmd_line.start..=cmd_line.end {
+        target_set.insert(i as i64);
+    }
+    println!("Time to setup target {:.2?}",before.elapsed());
+
+    let before = Instant::now();
+    let reader = BufReader::new(file);
+	let mut _count = 0;
+	let mut _count_valid = 0;
+	let mut _count_invalid = 0;
+    for line in reader.lines() {
+		_count += 1;	
+        if _count % 100000 == 0 {
+            println!("*")
+        }
+        else if _count % 1000 == 0 {
+            print!(".")
+        }
+		let line_data = line.unwrap();
+        let num = line_data.parse::<i64>().unwrap();
+        input_set.insert(num);
+        continue;
+
+        // find the the input number, ignoring leading whitespace
+        let re_input = Regex::new(r"^\s*(?P<number>-*\d+).*$").unwrap();
+
+        if let Some(x) = re_input.captures(&line_data) {
+               _count_valid += 1;	
+               let match_numstr = x.name("number").map_or("", |m| m.as_str());
+//               println!("match is {}",match_numstr);
+               let num = match_numstr.parse::<i64>().unwrap();
+               //println!("Found {}",num);
+//               input_set.insert(num);
+
+
+        }
+        else {
+           _count_invalid += 1;	
+            println!("Skipping line #{} {}",_count,line_data)
+        };
+
+    }
+    println!("Time to read {:.2?}",before.elapsed());
+    //exit(0);
+
+    println!("Input read {} lines {} valid, {} invalid",_count,_count_valid,_count_invalid);
+    println!("Resulting in  {} unique entries",input_set.len());
+    println!("Target size is {} ",target_set.len());
+
+
+    for t in target_set {
+//        println!("Looking for {}",t);
+        for x in &input_set {
+            let y = t - x;
+            if input_set.contains(&y) {
+                result_set.insert(t);
+                break;
+            }
+
+        }
+
+    }
+    println!("Output: Result size {}",result_set.len());
+
+}
+
+
+/*
+ * the rest of this file sets up unit tests
+ * to run these, the command will be:
+ * cargo test --package rust-template -- --nocapture
+ * Note: 'rust-template' comes from Cargo.toml's 'name' key
+ */
+/*
+// use the attribute below for unit tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+	fn setup_basic1() -> Graph {
+		let mut g = Graph::new();
+		assert_eq!(g.add_edge(1,2),Some(1));
+		assert_eq!(g.add_edge(1,3),Some(2));
+		assert_eq!(g.add_edge(2,3),Some(1));
+		assert_eq!(g.add_edge(2,4),Some(2));
+		assert_eq!(g.add_edge(3,4),Some(1));
+		assert_eq!(g.get_outgoing(1),&[2,3]);
+		assert_eq!(g.get_outgoing(2),&[3,4]);
+		assert_eq!(g.get_outgoing(3),&[4]);
+		assert_eq!(g.get_outgoing(4),&[]);
+		g
+	} 
+
+    #[test]
+    fn basic() {
+		let mut g = Graph::new();
+		assert_eq!(g.create_vertex(&1),Some(1));
+		assert_eq!(g.create_vertex(&2),Some(2));
+		assert_eq!(g.add_edge(1,2),Some(1));
+		assert_eq!(g.get_vertexes(),vec!(1,2));
+		assert_eq!(g.create_vertex(&3),Some(3));
+		assert_eq!(g.add_edge(1,3),Some(2));
+		assert_eq!(g.add_edge(2,3),Some(1));
+		assert_eq!(g.get_vertexes(),vec!(1,2,3));
+		assert_eq!(g.add_edge(1,4),Some(3));
+		assert_eq!(g.get_vertexes(),vec!(1,2,3,4));
+		println!("{:?}",g);
+
+    }
+
+	#[test]
+	fn test_add() {
+		let mut g = Graph::new();
+		assert_eq!(g.add_edge(1,2),Some(1));
+		assert_eq!(g.get_outgoing(1),&[2]);
+		assert_eq!(g.get_incoming(2),&[1]);
+		assert_eq!(g.add_edge(1,3),Some(2));
+		assert_eq!(g.get_outgoing(1),&[2,3]);
+		assert_eq!(g.get_incoming(2),&[1]);
+	}
+
+	#[test]
+	fn test_add_del() {
+		let mut g = setup_basic1();
+		assert_eq!(g.get_outgoing(1),&[2,3]);
+		assert_eq!(g.add_edge(1,2),Some(3));
+		assert_eq!(g.get_outgoing(1),&[2,3]);
+		assert_eq!(g.get_outgoing(2),&[3,4]);
+		assert_eq!(g.get_outgoing(3),&[4]);
+		assert_eq!(g.delete_edge(1,2),Ok(()));
+		assert_eq!(g.get_outgoing(1),&[2,3]);
+		assert_eq!(g.delete_edge(1,2),Ok(()));
+		assert_eq!(g.get_outgoing(1),&[3]);
+		
+	}
+
+
+ }
+ */
